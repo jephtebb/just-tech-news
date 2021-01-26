098f08dba8dd387b68874145ba16d6de499b4781
Jephtebb <jephteborno@yahoo.com>
2021-01-25 23:18:07 -0500

    secure password

--- models/User.js
+++ models/User.js
@@ -2,7 +2,13 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 // create our User model
-class User extends Model {}
+// create our User model
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 // create fields/columns for User model
 User.init(
