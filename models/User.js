98c380cd363aaa26df895a578e8cda0fc62ac0d1
Jephtebb <jephteborno@yahoo.com>
2021-01-27 19:02:32 -0500

    Update GET Routes to Include Votes and Refactor Messy Code

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
-// create our User model
+const sequelize = require('../config/connection');
+
 // create our User model
 class User extends Model {
   // set up method to run on instance data (per user) to check password
@@ -46,7 +46,7 @@ User.init(
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
       },
-      // set up beforeUpdate lifecycle "hook" functionality
+
       async beforeUpdate(updatedUserData) {
         updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
         return updatedUserData;
